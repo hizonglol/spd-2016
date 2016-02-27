d407c9c32efb464182896bb0b8e93801b8ba9b1e
hizonglol <209193@student.pwr.wroc.pl>
2016-02-27 12:11:53 +0100

    Poprawka nr 1
    Dodane funkcje wczytujące i zapisujące. Dodane zabezpieczenia w
    funkcjach przed błędami.

--- cw1.cpp
+++ cw1.cpp
@@ -5,16 +5,18 @@
 #include "tools.h"
 
 using namespace std;
+/* zrodla do poczytania
 
-int main()
-{
+http://dominik.zelazny.staff.iiar.pwr.wroc.pl/pliki/spd/zad1/Zadanie_1.pdf
+
+*/
+
+int main() {
 	vector< vector<int> > zad1;
 	unsigned zad1_length;
 
 	tools::load(zad1_length, zad1);
-
-	
-
+	tools::save(zad1);
 	tools::pause();
 	return 0;
 }
--- tools.h
+++ tools.h
@@ -7,48 +7,77 @@
 namespace tools {
 
 	//PAUSE *********************************************
-	void pause()
-	{
-		do
-		{
+	const void pause() {
+		do {
 			std::cout << "\n" << "Press the Enter key to continue.";
-		}
-		while (std::cin.get() != '\n');
+		} while (std::cin.get() != '\n');
 	}
 
 	//LOAD *********************************************
-	int load(unsigned& zad_length, std::vector< std::vector<int> > &zad)
-	{
+	const int load(unsigned& zad_length, std::vector< std::vector<int> > &zad) {
 		std::ifstream file;
 		file.open("dane.txt", std::ios::in);
 		
-		if (file.fail())
-		{
-			std::cout << "File didn't open properly";
+		if (file.fail()) {
+			std::cerr << "File didn't open properly within tools::load()";
 			return 1;
 		}
 		
-		if (file.is_open())
-    	{
-    		unsigned i;
+		if (file.is_open()) {
+    		//! to do optymalizacji
+    		unsigned counter = 1;
+    		unsigned r, p, q;
 			file >> zad_length;
-			std::cout << zad_length;
-			file.close();
 			
-			while(!file.eof())
-			{
-			
+			while (counter <= zad_length) {
+				std::vector<int> temp;
+				file >> r;
+				file >> p;
+				file >> q;
+				temp.push_back(counter);
+				temp.push_back(r);
+				temp.push_back(p);
+				temp.push_back(q);
+				zad.push_back(temp);
+				++counter;
 			}
+			
+			file.close();
 		
    		return 0;
     	}
     	
-		std::cerr << "Unidentified error";
+		std::cerr << "Unidentified error within tools::load()";
+		return -1;
+	}
+	
+	const int save(std::vector< std::vector<int> > const &zad) {
+		std::ofstream file;
+		file.open("posortowane.txt", std::ios::out);
+	
+		if (file.fail()) {
+			std::cerr << "File didn't open properly within tools::save()";
+			return 1;
+		}
+	
+		if (file.is_open()) {
+		
+			for (int i=0; i < zad.size(); ++i){
+				for (int j=1; j < zad[1].size(); ++j){
+					file << zad[i][j];
+					file << ' ';
+				}
+				file << std::endl;
+			}
+		return 0;
+		}
+		
+		std::cerr << "Unidentified error within tools::save()";
 		return -1;
 	}
 	
 	//C_MAX *********************************************
-	int c_max(unsigned& zad_length, std::vector< std::vector<int> > &zad) {
+	const int c_max(unsigned& const zad_length, std::vector< std::vector<int> > const &zad) {
 		
 		
 		return 0;
