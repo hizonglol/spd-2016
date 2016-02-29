83d65c2c35c0c3452af5665000a5af1dfc8101ca
Wojciech <194450@student.pwr.wroc.pl>
2016-02-29 20:33:43 +0100

    Algorytm i wczytywanie
    Dokończyłem ten algorytm, ale na razie nie daje za dobrego czasu. Możesz
    sprawdzić, bo dodałem zmienną string z nazwą pliku z danymi i same
    pliki. Trzeba jeszcze zrobić automatyczne 4 symulacje i zsumować czasy.
    Pomyślę nad szybszym algorytmem.

--- cw1.cpp
+++ cw1.cpp
@@ -16,12 +16,30 @@ http://czeslaw.smutnicki.staff.iiar.pwr.wroc.pl/sterowanie%20procesami%20dyskret
 
 int main() {
 	vector<vector<unsigned> > zad1;
+	vector<vector<unsigned> > sorted;
 	unsigned zad1_length;
+	string wejscie;
+	wejscie = "dane2.txt";
 
-	tools::load(zad1_length, zad1);
+	tools::load(zad1_length, zad1, wejscie);
 	tools::save(zad1);
 	cout << "Czas calkowity: " << tools::c_max(zad1_length, zad1) << endl;
+	tools::sort_1(zad1);
+	cout << "Czas calkowity po sortowaniu 1: " << tools::c_max(zad1_length, zad1) << endl;
+	tools::print(zad1);
+	tools::sort_2(zad1);
+	cout << "Czas calkowity po sortowaniu 2: " << tools::c_max(zad1_length, zad1) << endl;
+	tools::print(zad1);
+	tools::sort_3(zad1);
+	cout << "Czas calkowity po sortowaniu 3: " << tools::c_max(zad1_length, zad1) << endl;
+	tools::print(zad1);
+	tools::sort_schrage(zad1_length, zad1, sorted);
+	cout << "Czas calkowity po sortowaniu Schrage'a: " << tools::c_max(zad1_length, sorted) << endl;
+	tools::print(sorted);
+	
+
 	tools::clean(zad1_length, zad1);
+	tools::clean(zad1_length, sorted);
 	
 	tools::pause();
 	return 0;
