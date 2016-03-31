3674fd50fa445aab651bde87d73f15dd8d20c69d
hizonglol <209193@student.pwr.edu.pl>
2016-03-31 20:35:59 +0200

    Witi update
    Witi działa na 4.5

--- tools.h
+++ tools.h
@@ -9,7 +9,7 @@ namespace tools {
 	
 	
 	//LOAD ******************************************************************************************
-	short load(unsigned& zad_length, std::vector<std::vector<unsigned> >& zad, std::string wejscie) {
+	short load(unsigned zad_length, std::vector<std::vector<unsigned> >& zad, std::string wejscie) {
 		std::ifstream file;
 		file.open(wejscie.c_str(), std::ios::in);
 		
@@ -89,7 +89,7 @@ namespace tools {
 	
 	
 	//CLEAN *****************************************************************************************
-	short clean(unsigned& zad_length, std::vector< std::vector<unsigned> >& zad) {
+	short clean(unsigned zad_length, std::vector< std::vector<unsigned> >& zad) {
 		
 		for (unsigned i=0; i < zad_length; ++i) {
 			zad[i].clear();
@@ -108,21 +108,252 @@ namespace tools {
 	}
 	
 	/* kara i-tego zadania bez kar poprzedzajacych */
-	unsigned long i_penalty(int const& c_temp, std::vector<unsigned> task) {
-		
-		
-		return (std::max<unsigned long>(0, c_temp+task[1]-task[3])*task[2]);
+	int i_penalty(unsigned c_temp, std::vector< std::vector <unsigned> > const& task, int k) {
+		if (c_temp > task[k][3]){
+			return (c_temp - task[k][3])* task[k][2];
+		}
+		else return 0;
 	}
 	
+	/* kara calkowita dla wektora wejsciowego */
 	unsigned long sum_penalty(std::vector< std::vector<unsigned> > const& tasks){
 		unsigned long sum = 0;
 		unsigned long c_temp = 0;
 		
 		for (unsigned i = 0; i<tasks.size(); ++i){
-			sum += ( std::max<unsigned long>(0,c_temp+tasks[i][1]-tasks[i][3]) )*tasks[i][2];
 			c_temp += tasks[i][1];
+			if(c_temp > tasks[i][3]) sum += (c_temp - tasks[i][3])*tasks[i][2];
 		}
 		return sum;
+	}
+	
+	/* czas wybranych zadan */
+	unsigned long time(int i, std::vector< std::vector<unsigned> > const& tasks){
+		int k, j;
+		int time = 0;
+		for(j = 1, k = 0;j <= i;j<<=1,k++){
+			if(j&i) time += tasks[k][1];
+		}
+		return time;
+	}
+	
+	/* czas calkowity dla wektora wejsciowego */
+	unsigned long sum_time(std::vector< std::vector<unsigned> > const& tasks){
+		unsigned long sum = 0;
+		for (unsigned i = 0; i<tasks.size(); ++i) sum += tasks[i][1];
+		return sum;
+	}
+	
+	void print1d(std::vector< long int > const& task) {
+		std::cout << "Drukuje\n";
+		for(unsigned int i = 0; i < task.size(); i++){
+			std::cout << task[i] << std::endl;
+		}
+	}
+	
+	void print2d(std::vector< std::vector < long int > > const& task, int pole) {
+		std::cout << "Drukuje pole " << pole << std::endl;
+		for(unsigned int i = 0; i < task.size(); i++){
+			std::cout << task[i][pole] << std::endl;
+		}
+	}
+	
+	/* zamienia wektor jednowymiarowy indeksow na wektor zadan */
+	void swap_index(std::vector< long int > const& index_l, std::vector< std::vector<unsigned> >& tasks, unsigned n) {
+		std::vector< int > indeksy;
+		std::vector< std::vector<unsigned> > temp_vec;
+		indeksy.resize(n);
+		temp_vec.resize(n);
+		unsigned indeks;
+		unsigned temp = index_l.size() - 1;
+		
+		for(unsigned int i = n; i; i--){
+			indeks = index_l[temp];
+			indeksy[i-1] = indeks;
+			temp = temp - (1uL<<(indeks-1));
+		}
+		
+		for(unsigned int i = 0; i < indeksy.size(); i++){
+			temp_vec[i] = tasks[indeksy[i]-1];
+		}
+		
+		tasks.swap(temp_vec);
+	}
+	
+	/* sortuje wektor zadan krytycznych aby uzyskac rozwiazanie pierwsze topologicznie */
+	void sort_vector(std::vector< std::vector<unsigned> >& tasks1) {
+		unsigned zapas = 0;
+		unsigned time = 0;
+		unsigned poz = 0;
+		unsigned poz2 = 0;
+		unsigned index;
+		unsigned min_task;
+		unsigned min_zapas;
+		unsigned i, j;
+		bool warunek;
+		
+		for(poz = 0; poz < tasks1.size(); poz++){
+			time = 0;
+			zapas = 0;
+			min_zapas = 99999;
+			min_task = 99999;
+			index = 99999;
+			warunek = false;
+			
+			// liczenie minimalnego czasu przetwarzania dla zadan po pozycji startowej
+			for(j = poz+1; j < tasks1.size(); j++){
+				if(tasks1[j][1] < min_task)	min_task = tasks1[j][1];
+			}
+//			std::cout << "Minimalny czas tasks1 " << min_task << std::endl;
+			
+			// liczenie czasu zadan przed pozycja startowa
+			for(i = 0; i < poz; i++){
+				time += tasks1[i][1];
+			}
+//			std::cout << "Czas przed " << time << std::endl;
+			// liczenie zapasu zadania startowego
+			time += tasks1[poz][1];
+			std::cout << "Czas startu " << time << std::endl;
+			if(tasks1[poz][3] > time){
+				min_zapas = tasks1[poz][3] - time;
+			}
+			else min_zapas = 0;	
+			
+//			std::cout << "Zadanie startu " << tasks1[poz][0] << std::endl;
+//			std::cout << "Zapas zad startu " << min_zapas << std::endl;
+
+			// szukanie zadania o najmniejszym indeksie, ktore mozna wstawic przed zadanie startowe
+			if(min_zapas >= min_task){
+				for(i = poz+1; i < tasks1.size(); i++){
+					time += tasks1[i][1];
+//					std::cout << "Czas po starcie " << time << std::endl;
+//					std::cout << "Limit zadania " << tasks1[i][3] << std::endl;
+					if(tasks1[i][3] >= time){
+						zapas = tasks1[i][3] - time;
+						if(zapas < min_zapas) min_zapas = zapas;						
+//						std::cout << "Zapas " << min_zapas << "  Numer zad startu: "  << tasks1[poz][0] << std::endl;
+//						std::cout << "Numer zadania: " << tasks1[i][0] << "  Index: " << index << std::endl;
+//						std::cout << "Limit zadania: " << tasks1[i][3] << "  Min zapas: " << min_zapas << std::endl;
+						
+						// jezeli numer zadania jest mniejszy i czas przetwarzania jest mniejszy od min zapasu
+						if(tasks1[i][0] < index && tasks1[i][0] < tasks1[poz][0] && tasks1[i][1] <= min_zapas){
+							index = tasks1[i][0];
+							poz2 = i;
+							warunek = true;
+//							std::cout << "Wybrano zadanie " << index << "  na pozycji " << poz2 << std::endl;
+						}
+					}
+				}
+			}
+			
+//			std::cout << "Start: " << poz << std::endl;
+//			std::cout << "Zadanie poczatkowe: " << tasks1[poz][0] << std::endl;
+//			std::cout << "Zadanie: " << poz2 << std::endl;
+//			std::cout << "Zadanie wybrane: " << tasks1[poz2][0] << std::endl;
+		
+			// wstawianie znalezionego zadania przed zadanie startowe	
+			if(warunek) {
+//				std::cout << "Pozycja poz2 " << poz2 << std::endl;
+//				tools::print(tasks1);
+				tasks1.insert(tasks1.begin()+poz,tasks1[poz2]);
+//				tools::print(tasks1);
+//				std::cout << "Zapas przed " << min_zapas << std::endl;
+				min_zapas = min_zapas - tasks1[poz2][1];
+//				std::cout << "Zapas po " << min_zapas << std::endl;
+				tasks1.erase(tasks1.begin()+poz2+1);
+//				tools::print(tasks1);
+				poz--;
+			}
+			//	else warunek = false;
+			
+			tools::print(tasks1);
+//			pause();
+		}
+	}	
+	
+	/* laczy wektory zadan tak, aby uzyskac rozwiazanie pierwsze topologicznie */
+	void merge(std::vector< std::vector<unsigned> >& tasks1, std::vector< std::vector<unsigned> >& tasks2) {
+		unsigned zapas = 0;
+		unsigned time = 0;
+		unsigned poz = 0;
+		unsigned poz2 = 0;
+		unsigned min_task;
+		unsigned min_zapas;
+		unsigned i, j;
+		bool warunek = true;
+		
+		while(warunek){
+			time = 0;
+			zapas = 0;
+			min_zapas = 99999;
+			min_task = 99999;
+			for(j = 0; j < tasks2.size(); j++){
+				if(tasks2[j][1] < min_task)	min_task = tasks2[j][1];
+			}
+//			std::cout << "Minimalny czas tasks2 " << min_task << std::endl;
+			
+			for(i = 0; i < tasks1.size(); i++){
+				time += tasks1[i][1];
+				if(tasks1[i][3] > time){
+					zapas = tasks1[i][3] - time;
+				}
+				else zapas = 0;
+				
+				if(min_zapas == 0 && zapas >= min_task){
+					min_zapas = zapas;
+					if(poz<i) poz = i;
+				}
+				
+				if(zapas < min_zapas){
+					min_zapas = zapas;
+//					poz = i;
+				}
+				if(zapas < min_task){
+					min_zapas = 0;				
+				}
+
+//				std::cout << "Numer zadania " << tasks1[i][0] << "  Pozycja zadania " << i << std::endl;
+//				std::cout << "Czas " << time << std::endl;
+//				std::cout << "Zapas " << zapas << std::endl;
+//				std::cout << "Minimalny zapas " << min_zapas << std::endl;
+//				std::cout << "Zadanie " << poz << std::endl;
+			}
+			
+//			std::cout << "Czas " << time << std::endl;
+//			std::cout << "Zapas " << min_zapas << std::endl;
+//			std::cout << "Zadanie " << poz << std::endl;
+			
+			if(poz < tasks1.size()) poz2 = tasks1[poz][0];
+			
+			if(min_zapas < min_task || tasks2.size() == 1 || poz == tasks1.size() - 1){
+				warunek = false;
+				while(!tasks2.empty()){
+					tasks1.push_back(tasks2[0]);
+					tasks2.erase(tasks2.begin());
+				}	
+			} 
+			else{
+				for(j = 0; j < tasks2.size(); j++){
+					if(tasks2[j][1] <= min_zapas && tasks2[j][0] < poz2) {
+//						tools::print(tasks1);
+						tasks1.insert(tasks1.begin()+poz,tasks2[j]);
+//						std::cout << "Zapas przed " << min_zapas << std::endl;
+						min_zapas = min_zapas - tasks2[j][1];
+//						std::cout << "Zapas po " << min_zapas << std::endl;
+						tasks2.erase(tasks2.begin()+j);
+//						tools::print(tasks1);
+						poz++;
+						j--;
+					}
+				}
+				poz++;
+			}
+			
+//			tools::print(tasks1);
+//			tools::print(tasks2);
+			
+//			pause();
+		}
 	}
 	
 }
--- witi.cpp
+++ witi.cpp
@@ -8,35 +8,71 @@
 using namespace std;
 
 int main(){
+	int loop;
+	string wejscie1 = "dane_a.txt";
 	
-std::vector< std::vector<unsigned> > tasks1;
-unsigned tasks1_length;
-string wejscie1 = "dane1.txt";
-std::vector< std::vector<unsigned long> > penalties;
+	for(loop = 97; loop < 108; loop++){
+		wejscie1[5] = loop;
+		cout << "Wczytano " << wejscie1 << endl;
+		
+		unsigned long limit;
+		unsigned c_max;
+		int i, j, k, pen, index;
+		int long min_kara;		
+		
+		/* wektor drugi zawiera zadania, ktore nie wygeneruja kary niezaleznie od pozycji */
+		vector< vector<unsigned> > tasks, tasks1, tasks2;
+		vector< long int > penalties;
+		vector< long int > indeks;	
+		tools::load(tasks.size(), tasks, wejscie1);
+		
+		/* ustalanie limitu czasowego wzgledem ktorego dzielone sa wektory */
+		limit = tools::sum_time(tasks);
+		
+		/* dzielenie wektora na dwa */
+		for(unsigned l = 0; l < tasks.size(); ++l){
+			if(tasks[l][3] < limit) tasks1.push_back(tasks[l]);
+			else tasks2.push_back(tasks[l]);
+		}
+		
+		int r=1uL<<tasks1.size();
+		penalties.resize(r);
+		indeks.resize(r);
+		penalties[0] = 0;
+		indeks[0] = 0;
+		
+		/* algorytm szukajacy rozwiazania optymalnego po wektorze 1 */
+		for (i = 1; i < r; ++i){
+			min_kara = 999999;
+			c_max = 0;
+			c_max = tools::time(i, tasks1);
+			for(j = 1,k = 1;j <= i;j<<=1){
+				if(i&j){
+					pen = penalties[i-j] + tools::i_penalty(c_max, tasks1, k-1);
+					if(pen<min_kara){
+						min_kara = pen;
+						index = k;
+					}
+				}
+				k++;
+			}
+			penalties[i] = min_kara;
+			indeks[i] = index;
+		}
+		
+		tools::swap_index(indeks,tasks1,tasks1.size());	
+		tools::merge(tasks1,tasks2);
+		tools::print(tasks1);
 
-tools::load(tasks1_length, tasks1, wejscie1);
-
-int r=1uL<<tasks1.size();
-
-std::vector<unsigned long> temp;
-temp.push_back(0);
-temp.push_back(0);
-temp.push_back(0);
-penalties.push_back(temp);
-
-//int c_max = 0;
-
-for (int i = 1; i <= r; ++i){
-	temp[0] = i;
-	
-	//temp[1] = tools::penalty(c_max, );
-	
-	//penalties.push_back(p);
-	//c_max += ;
-}
-	std::cout << tools::sum_penalty(tasks1);
-
-tools::pause();
-	
+		cout << "Kara: " << penalties.back() << endl;
+		cout << "Kara alternatywna: " << tools::sum_penalty(tasks1) << endl << endl;
+		
+		tools::clean(tasks1.size(),tasks1);
+		tools::clean(tasks2.size(),tasks2);
+		tools::clean(tasks.size(),tasks);
+		penalties.clear();
+		indeks.clear();
+	}
+	tools::pause();	
 	return 0;
 }
