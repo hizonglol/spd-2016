99290e5c234acb7a1eb2bdd94c6df2ef78b297e9
hizonglol <209193@student.pwr.edu.pl>
2016-05-12 14:32:07 +0200

    Niedziałający carlier

---
+++ carlier.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+
+#include "tools.h"
+
+using namespace std;
+/* jak powinno wyjsc dla tych danych:
+Schrage:
+1. 13981   
+2. 21529   
+3. 31683   
+4. 34444   
+
+Carlier:
+1. 13862
+2. 20917
+3. 31343
+4. 33878
+*/
+
+int main() {
+	vector<vector<unsigned> > zad1;
+	//vector<vector<unsigned> > zad2;
+	//vector<vector<unsigned> > zad3;
+	//vector<vector<unsigned> > zad4;
+	unsigned zad1_length;
+	//unsigned zad2_length;
+	//unsigned zad3_length;
+	//unsigned zad4_length;
+	string wejscie1 = "dane1.txt";
+	//string wejscie2 = "dane2.txt";
+	//string wejscie3 = "dane3.txt";
+	//string wejscie4 = "dane4.txt";
+	unsigned U1 = 255565;
+	//unsigned U2, U3, U4;
+	//U4 = U3 = U2 = U1;
+	//unsigned suma;
+
+	tools::load(zad1_length, zad1, wejscie1);
+	
+	tools::carlier(zad1_length, zad1, U1);
+	cout << "Czas Carliera dla danych 1: " << tools::c_max(zad1_length, zad1) << endl;
+	
+	//cout << tools::preschrage(zad1_length,zad1) << endl;
+	tools::print(zad1);
+	
+	/*
+	tools::load(zad2_length, zad2, wejscie2);
+	tools::carlier(zad1_length, zad1, U2);
+	cout << "Czas Carliera dla danych 2: " << tools::c_max(zad2_length, zad2) << endl;
+	tools::print(zad2);
+	
+	tools::load(zad3_length, zad3, wejscie3);
+	tools::carlier(zad1_length, zad1, U3);
+	cout << "Czas Carliera dla danych 3: " << tools::c_max(zad3_length, zad3) << endl;
+	tools::print(zad3);
+	
+	tools::load(zad4_length, zad4, wejscie4);
+	tools::carlier(zad1_length, zad1, U4);
+	cout << "Czas Carliera dla danych 4: " << tools::c_max(zad4_length, zad4) << endl;
+	tools::print(zad4);
+
+	suma = tools::c_max<unsigned>(zad1_length, zad1) + tools::c_max(zad2_length, zad2) + tools::c_max(zad3_length, zad3) + tools::c_max(zad4_length, zad4);
+	cout << "Suma czasow: " << suma << endl;
+*/
+	tools::save(zad1); //zapisujemy do pliku permutacje zadan
+	tools::clean(zad1_length, zad1); //sprzatamy wektory
+	
+	tools::pause();
+	return 0;
+}
--- tools.h
+++ tools.h
@@ -15,24 +15,8 @@ namespace tools {
 		while (std::cin.get() != '\n');
 	}
 
-	
-	/* komparatory do Schrage *///*******************************************************************
-	struct compare_r {
-        bool operator()(std::vector<unsigned> const& t1, std::vector<unsigned> const& t2) {
-           	return t1[1] > t2[1];
-        	}
-   		};
-   	
-	struct compare_q {
-        bool operator()(std::vector<unsigned> const& t1, std::vector<unsigned> const& t2) {
-           	return t1[3] < t2[3];
-        	}
-   		};
-
 	//LOAD ******************************************************************************************
-	//short load(unsigned& zad_length, std::vector<std::vector<unsigned> >& zad, std::string wejscie) {
-	short load(std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_r> & N, std::string wejscie) {
-		unsigned zad_length;
+	short load(unsigned& zad_length, std::vector<std::vector<unsigned> >& zad, std::string wejscie) {
 		std::ifstream file;
 		file.open(wejscie.c_str(), std::ios::in);
 		
@@ -42,6 +26,7 @@ namespace tools {
 		}
 		
 		if (file.is_open()) {
+    		//! to do optymalizacji
     		unsigned counter = 1;
     		unsigned r, p, q;
 			file >> zad_length;
@@ -55,7 +40,7 @@ namespace tools {
 				temp.push_back(r);
 				temp.push_back(p);
 				temp.push_back(q);
-				N.push(temp);
+				zad.push_back(temp);
 				++counter;
 			}
 			
@@ -69,9 +54,9 @@ namespace tools {
 	}
 
 	//SAVE ******************************************************************************************
-	short save(std::vector< std::vector<unsigned> > const& zad, std::string wyjscie) {
+	short save(std::vector< std::vector<unsigned> > const& zad) {
 		std::ofstream file;
-		file.open(wyjscie.c_str(), std::ios::out);
+		file.open("posortowane.txt", std::ios::out);
 	
 		if (file.fail()) {
 			std::cerr << std::endl << "File didn't open properly within tools::save()";
@@ -152,88 +137,251 @@ namespace tools {
 		return -1;
 	}
 
-
+	
+	/* komparatory do Schrage *///*******************************************************************
+	struct compare_r {
+        bool operator()(std::vector<unsigned> const& t1, std::vector<unsigned> const& t2) {
+           	return t1[1] > t2[1];
+        }
+   	};
+   	
+	struct compare_q {
+        bool operator()(std::vector<unsigned> const& t1, std::vector<unsigned> const& t2) {
+           	return t1[3] < t2[3];
+        }
+   	};
 	
 	/*
-	schrage() napisany na podstawie algorytmu podanego na stronie:
+	schrage() oraz preshrage() napisany na podstawie algorytmu podanego na stronie:
 	http://dominik.zelazny.staff.iiar.pwr.wroc.pl/materialy/Algorytm_Schrage.pdf
 	*/
 	
 	/* schrage *///**********************************************************************************
-	//int schrage (std::vector< std::vector<unsigned> > & tasks) {
-	int schrage (std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_r> & N) {
+	int schrage (std::vector< std::vector<unsigned> > & tasks) {
 		unsigned t = 0;
 		unsigned c_max = 0;
-		std::vector<std::vector<unsigned> > pi;
+		std::vector<std::vector<unsigned> > pi; //permutacja zadan po sortowaniu
 		
-		std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_q> G;
+		std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_r> N; //kolejka min(r)
+		std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_q> G; //kolejka max(q)
 		
-		while ((!G.empty()) || (!N.empty())) {
+		while (!tasks.empty()) {
+			N.push(tasks.back()); //wrzucamy zadania do kolejki min(r)
+			tasks.pop_back(); //kasujemy zadania do posortowania
+		}
+		
+		while ((!G.empty()) || (!N.empty())) { //dopoki wszystkie zadania nie zostaly przetworzone
 			
-			while ((!N.empty()) && (N.top()[1] <= t)) {
-				G.push(N.top());
-				N.pop();
+			while ((!N.empty()) && (N.top()[1] <= t)) { //jesli jest zadanie co "p" ma mniejsze od momentu czasowego
+				G.push(N.top()); //wrzucamy nowe zadanie do kolejki max(q)
+				N.pop(); //i kasujemy z kolejki min(r)
 			}
 			
-			if (G.empty()) {
-				t = N.top()[1];
+			if (G.empty()) { //jesli nie ma zadnego zadania przygotowanego do przetwarzania
+				t = N.top()[1]; //przesuwamy sie o odpowiedni czas
 			}
 			else {
-			t += G.top()[2];
-			c_max = std::max<int>(c_max, t + G.top()[3]);
-			pi.push_back(G.top());
-			G.pop();
+			t += G.top()[2]; //przesuwamy sie z czasem "t" do przodu
+			c_max = std::max<int>(c_max, t + G.top()[3]); //zwiekszamy "c_max"
+			pi.push_back(G.top()); //zapisujemy wykonane zadanie w permutacji "pi"
+			G.pop(); //kasujemy zadanie z kolejki max(q)
 			}
 		}
+		
+		tasks = pi; //wpisujemy w wektor do posortowania permutacje zadan posortowanych
 	
 		return c_max;
 	}
 	
-	/* schrage z przerwaniami *///**********************************************************************************
-	int schrage_interrupt (std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_r> & N) {
-		unsigned t = 0;
+	/* schrage z przerwaniami *///*******************************************************************
+	int preschrage (unsigned const& zad_length, std::vector< std::vector<unsigned> >const & tasks) {
+		unsigned t = 0; //aktualny moment czasowy
 		unsigned c_max = 0;
-		std::vector<unsigned> l;
-		l.push_back(0);
-		l.push_back(0);
-		l.push_back(0);
-		l.push_back(10000);
+		std::vector<unsigned> machine_task; //zmienna przechowujaca zadanie wykonywane na maszynie
+		machine_task.push_back(0); 
+		machine_task.push_back(0);
+		machine_task.push_back(0);
+		machine_task.push_back(100000); //tworzymy zadanie poczatkowe
 		
-		std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_q> G;
+		std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_r> N; //kolejka min(r)
+		std::priority_queue <std::vector<unsigned>, std::vector< std::vector<unsigned> >, compare_q> G; //kolejka max(q)
 		
-		while ((!G.empty()) || (!N.empty())) {
+		for (unsigned i = 0; i<zad_length; ++i) {
+			N.push(tasks[i]); //wrzucamy zadania do kolejki min(r)
+		}
+		
+		while ((!G.empty()) || (!N.empty())) { //dopoki wszystkie zadania nie zostaly przetworzone
 			
-			while ((!N.empty()) && (N.top()[1] <= t)) {
-				G.push(N.top());
+			while ((!N.empty()) && (N.top()[1] <= t)) { //jesli jest zadanie co "p" ma mniejsze od momentu czasowego
+				G.push(N.top()); //wrzucamy nowe zadanie do kolejki max(q) ale zostawiamy do porownania z zadaniem na maszynie
 				
-				if(N.top()[3] > l[3]){
-					
-					l[2] = t - N.top()[1];
-					t = N.top()[1];
-					
-					if(l[2] > 0){
-						G.push(l);
+				if (N.top()[3] > machine_task[3]) { //jesli dorzucone zadanie ma wieksze "q" od zadania na maszynie
+					machine_task[2] = t - N.top()[1]; //zmniejszamy zadanie na maszynie o tyle ile sie wykonalo
+					t = N.top()[1]; //nowy czas jest czasem "p" zadania nowo przybylego
+					if (machine_task[2] > 0) {
+						G.push(machine_task); //jesli zadanie na maszynie nie skonczone to z powrotem do kolejki max(q)
 					}
 				}
 				
-				N.pop();
+				N.pop(); //po porownaniu z zadaniem na maszynie kasujemy
 			}
-			
-			if (G.empty()) {
-				t = N.top()[1];
+
+			if (G.empty()) { //jesli nie ma zadnego zadania przygotowanego do przetwarzania
+				t = N.top()[1]; //przesuwamy sie o odpowiedni czas
 			}
 			else {
-			l = G.top();
-			t = t + G.top()[2];
-			c_max = std::max<int>(c_max, t + G.top()[3]);
+			machine_task = G.top(); //wrzucamy na maszyne zadanie z kolejki max(q)
 			G.pop();
+			t += machine_task[2]; //przesuwamy sie z czasem "t" do przodu
+			c_max = std::max<int>(c_max, t + machine_task[3]); //zwiekszamy "c_max"
 			}
 		}
-	
+		
 		return c_max;
 	}
-
-/* koniec namespace tools *///***********************************************************************
+	
+	/*
+	set_a_b_index(), c_not_exists(), carlier() napisane na podstawie algorytmu podanego na stronie:
+	http://dominik.zelazny.staff.iiar.pwr.wroc.pl/materialy/Algorytm_Carlier.pdf
+	*/
+	
+	/* wyznaczanie sciezki krytycznej o indeksach a i b *///************************************************************************
+	void set_a_b_c_index(unsigned const& n, std::vector<std::vector<unsigned> > const& pi, long unsigned const& U, unsigned & a_index, unsigned & b_index){
+		a_index = 0;
+		b_index = 0;
+		unsigned j, s;
+		unsigned sum;
+		unsigned t_p = 0;
+		unsigned c_temp = 0;
+		
+		std::cout << "Wyznaczam b." << std::endl;
+		
+		for (j=0; j<n; j++){ //przeszukujemy wszystkie zadania z permutacji "pi"
+			t_p = std::max<unsigned long>(t_p,pi[j][1]) + pi[j][2];
+			c_temp = std::max<unsigned long>(t_p + pi[j][3], c_temp); //tutaj zwiekszam sobie c_max dla j-tego elementu
+			
+			if (c_temp == U){
+				std::cout << "Indeks b: "  << j << std::endl;
+				b_index = j;
+			}
+		}
+		
+		std::cout << "Wyznaczam a." << std::endl;
+		
+		sum = 0;
+		for (s=0; s<=b_index; s++)	sum += pi[s][2]; //suma "p" od 0 do "b_index"
+		
+		for (j=0; j<n; j++){ //przesuwamy sie po wszystkich elementach wektora
+			if (pi[j][1] + sum + pi[b_index][3] <= U) { //sprawdzamy warunek
+				a_index = j;
+				std::cout << "Indeks a: " << a_index << std::endl;
+				break; //pierwsze znalezione "a_index" konczy petle
+			}
+			
+			sum -= pi[j][2]; //zmieniamy przedzial sumy kiedy zmienia sie "a"
+		}
+	}
+	
+	/* wyznaczanie c *///****************************************************************************
+	bool c_exists(std::vector<std::vector<unsigned> > const& pi, unsigned const& a_index, unsigned const& b_index, unsigned& c_index) {
+		bool c_exists = false; //c domyslnie nie istnieje
+		
+		for (unsigned j=b_index; j>a_index; --j){ //poruszamy sie po sciezce krytycznej
+			if (pi[j][3] < pi[b_index][3]){ //szukamy zadania o najwyzszym indeksie poprzedzajacego "b_index" z krotszym "q" od "b_index"
+				c_exists = true; //c istnieje
+				c_index = j; //jesli znalezione zadanie c to zapisujemy jego indeks w "c_index"
+				break;
+			}
+		}
+		
+		return c_exists;
+	}
+	
+	/* carlier *///**********************************************************************************
+	int carlier(unsigned const& n, std::vector< std::vector<unsigned> > & tasks, unsigned& UB){
+		long unsigned U, LB;
+		unsigned r_old, q_old;
+		unsigned r_prim = 0;
+		unsigned p_prim = 0;
+		unsigned q_prim = 0;
+		unsigned a_index, b_index, c_index;
+		
+		std::vector<std::vector<unsigned> > pi = tasks;
+		
+		
+		U = schrage(pi);
+		std::cout << "Carlier 1." << std::endl;
+		
+		
+		if(U < UB){
+			UB = U;
+			tasks = pi;
+		}
+		std::cout << "Carlier 2." << std::endl;
+		
+		
+		set_a_b_c_index(n, pi, U, a_index, b_index);
+		std::cout << "Carlier 3." << std::endl;
+		
+		
+		if (!c_exists(pi, a_index, b_index, c_index)){
+			std::cout << "C does not exist.";
+			std::cout << c_max(n, pi);
+			return 0;
+		}
+		std::cout << "Carlier 4." << std::endl;
+		
+		
+		p_prim = 0;
+		for (unsigned i=(c_index+1); i<b_index; ++i) {
+			if (pi[i][1] < r_prim) r_prim = pi[i][1];
+			if (pi[i][3] < q_prim) q_prim = pi[i][3];
+			p_prim += pi[i][2];
+		}
+		std::cout << "Carlier 5." << std::endl;
+		
+		
+		r_old = pi[c_index][1];
+		pi[c_index][1] = std::max<unsigned>(pi[c_index][1], r_prim+p_prim);
+		std::cout << "Carlier 6." << std::endl;
+		
+		
+		LB = preschrage(n, pi);
+		std::cout << "Carlier 7." << std::endl;
+		
+		
+		if (LB < UB) {
+		   std::cout << "Carlier 8." << std::endl; 
+		   carlier(n, pi, UB);
+		}
+		std::cout << "Carlier 9." << std::endl;
+		
+		
+		pi[c_index][1] = r_old;
+		std::cout << "Carlier 10." << std::endl;
+		
+		
+		q_old = pi[c_index][3];
+		pi[c_index][3] = std::max<unsigned>(pi[c_index][3], q_prim+p_prim);
+		std::cout << "Carlier 11." << std::endl;
+		
+		
+		LB = preschrage(n, pi);
+		std::cout << "Carlier 12." << std::endl;
+		
+		
+		if (LB < UB){
+		    std::cout << "Carlier 13." << std::endl; 
+		 	carlier(n, pi, UB);
+		}
+  		std::cout << "Carlier 14." << std::endl; 
+  		
+		
+		pi[c_index][3] = q_old;
+  		std::cout << "Carlier 15." << std::endl; 
+		
+		return -1;
+	}
 }
 
 #endif //TOOLS_H_INCLUDED
